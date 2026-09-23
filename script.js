6fcc74ffca3dfec08192d37e3dbbb87b553d908a
Mumpy <zephyrusyash@gmail.com>
2026-09-23 12:47:29 +0530

    Add human choice logic

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function getComputerChoice(){ //first function to get the choice of computer
     choice = Math.random() // the choice en is some random number whose value is stored in a variable 
 // now i  want to assign the range of these random value : rock , paper and scissors
 // for that i can use if choice between 0 to 0.3 hen return rock and so on...
-    if (choice >= 0 && choice < 0.3) {
+    if (choice >= 0 && choice < 0.5) {
     return "rock";
     }
-    else if ( choice >= 0.3 && choice < 0.6 ){
+    else if ( choice >= 0.5 && choice < 0.8 ){
         return "paper"
     }
     else{
@@ -20,3 +20,36 @@ let computerChoice = getComputerChoice();
 essentially my variable is now storing the computer choice in rock paper or scissors 
 this choice is triggered by the function getComputerChoice() and then stored in the 
 variable computerChoice. */
+
+
+
+
+//now the question i have in my mind is how is this function get triggered? because before this
+// we were passing arguments. and doing function calls.
+
+console.log(computerChoice);
+
+
+//human choice logic
+
+function getHumanChoice(){
+    let humanChoice = prompt("valid inputs : rock or paper or scissors")
+    if(humanChoice === "rock"){
+        return "rock";
+    }
+    else if (humanChoice === "paper"){
+        return "paper";
+    }
+    else if (humanChoice === "scissors"){
+        return "scissors";
+    }
+    else{
+        return "enter a valid input"
+    }
+}
+/* now we have written the logic to take user input 
+this block is basically taking user input using prompt method and storing that value in a variable 
+called humanChoice , then the if else block decides what will the code return after reading the value of 
+variable */
+
+console.log(getHumanChoice());
